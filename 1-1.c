fcf954223a67e7bf37a477f21dfbb2bc561d900d
3li724 <2595369900@qq.com>
2024-09-15 21:29:16 +0800

    second commit

--- 1-1.c
+++ 1-1.c
@@ -1,32 +1,16 @@
-/*
-假设输入到e时结束 
-*/ 
 #include<stdio.h>
 
+char a;
 int x,y,z;
-char a,goods[7];
-int s[7],num[7];
 
 int main(){
-	while(scanf("%c",&a)){
-		if(a=='e'){
-			for(int i=1;i<=5;i++)
-				if (num[i]) {
-					printf("%d: ",i);
-					for(int j=1;j<=num[i];j++) printf("%c",goods[i]);
-					printf(" %d\n",s[i]);
-				}
-			break;
-		}
-		if(a>=65&&a<=90){  //a可能读到回车 
-			scanf("%d%d%d",&x,&y,&z);
-			num[x]=z; s[x]=y; goods[x]=a;
-		}
-	}
+	scanf("%c%d%d%d",&a,&x,&y,&z);
+	printf("%d: ",x);
+	for(int j=1;j<=z;j++) printf("%c",a);
+	printf(" %d\n",y);
 	return 0;
 }
 /*
-
 A 3 7 5
 
 3: AAAAA 7
