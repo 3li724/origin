5df65cea2a361f7f91406b1a8fccadad1dcb95ed
3li724 <2595369900@qq.com>
2024-09-17 13:16:49 +0800

    second commit

--- 2-1.c
+++ 2-1.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
 
-int x,y,z,j,pr,sum;
-char a,goods[7];
+char a,b,goods[7];
+int x,y,z,j,pr,sum,step=1;
 int X[7],Y[7],s[7],num[7],buy[7];
+
+int END(){
+	if(a!='E') return 0;
+	b=getchar(); if(b!='N') return 0;
+	b=getchar();  return 1;
+}
 
 int end(){
 	for(int i=1;i<=5;i++)
@@ -13,7 +19,7 @@ int end(){
 void init(){
 	while(1){
 		a=getchar();
-		if(a=='e') return;
+		if(END()) return;
 		if(a<65||a>90) continue;
 		scanf("%d%d%d",&x,&y,&z);
 		goods[x]=a; num[x]=z; s[x]=y;
@@ -24,22 +30,17 @@ void select(){
 	j=pr=sum=0;
 	while(1){
 		a=getchar();
-		if(a=='e') break;
+		if(END()) break;
 		if(a<65||a>90) continue;
 		j++;  scanf("%d%d",&X[j],&Y[j]); //不能写成scanf("%d%d",&X[++j],&Y[j]);这样X,Y的值对应不起来 
 		sum+=s[X[j]]*Y[j];
 		if(buy[X[j]]+Y[j]>num[X[j]]) pr=1;
 	}
-	if(pr) {
-		printf("没货了,操作失败\n");
-		return; 
-	}
-	for(int i=1;i<=j;i++) buy[X[i]]+=Y[i];
-	return;
 }
 
 void pay(){
 	int k=0;
+	for(int i=1;i<=j;i++) buy[X[i]]+=Y[i];
 	while(scanf("%d",&x)){
 		k+=x;
 		if(k>=sum) {
@@ -52,29 +53,40 @@ void pay(){
 int main(){
 	init();
 	while(1){
-		select();
-		if(!pr) pay();
+		switch(step){
+			case 1:
+				select();
+				step=pr?2:3;
+			break;
+			case 2:
+				step=1;
+				printf("没货了,操作失败\n");	
+			break;
+			case 3:
+				pay();
+				step=1;
+			break;
+		}
 		if(end()) break;
 	}
 	return 0;
 }
 /*
-注：为操作方便，以'e'为一段输入结束的信号 
 in：
 A 3 7 5
 B 2 3 5
 B 1 3 3
-e
+END
 A 3 1
 B 2 1
-e
+END
 1
 5
 5
 B 1 3
 B 2 2
 A 3 3
-e
+END 
 5
 5
 5
@@ -84,10 +96,10 @@ e
 5
 5
 B 2 3
-e
+END
 A 3 1
 B 2 2
-e
+END
 5
 5
 5
