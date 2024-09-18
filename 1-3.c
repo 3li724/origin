8d11b694119ad9140259928fb55f87af1020a61d
3li724 <2595369900@qq.com>
2024-09-18 13:39:23 +0800

    second commit

--- 1-3.c
+++ 1-3.c
@@ -4,23 +4,23 @@ char a,b;
 int x,y,s,t,w,ex,num;
 
 int main(){
-	scanf("%c%d%d%d",&a,&ex,&w,&num); 
-	b=getchar();  //第一个回车
-	printf("请依次输入要买的货物,通道编号以及个数\n"); 
-	while(b=getchar()){
-		if(b<65||b>90) continue;  //防止以后读到回车
+	scanf("%c%d%d%d",&a,&ex,&w,&num);
+	while((b=getchar())!=EOF){
+		if(b<65||b>90) continue;
 		scanf("%d%d",&x,&y); 
 		if(num==0) break;
-		if(b!=a||x!=ex) { printf("没货了\n");  continue; }
-		if(y>num) {  printf("货不够了\n");  continue; }
-		printf("请投币\n");
+		if(b!=a||x!=ex||y>num) {
+			printf("操作失败\n");
+			continue;
+		}
 		while(1){
 			scanf("%d",&s);  t+=s;
-			if(t>=y*w) {  printf("找零%d元\n",t-y*w); break;}
-			printf("请继续投币\n");
+			if(t>=y*w) {
+				printf("%d\n",t-y*w);
+				break;
+			}
 		}
 		t=0; num-=y;
-		printf("请依次输入要买的货物,通道编号以及个数\n"); 
 	}
 	return 0;
 }
